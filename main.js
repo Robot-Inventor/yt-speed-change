323b866e83136e5aa611b2a2156cb11a98309a93
Robot-Inventor <30.keita@gmail.com>
2020-10-14 04:11:20 +0900

    最初にYouTubeのトップページを開くと正常に動作しないバグを修正

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 window.onload = function() {
+    default_speed = document.querySelector("video.video-stream.html5-main-video") ? document.querySelector("video.video-stream.html5-main-video").playbackRate : 1;
     document.body.insertAdjacentHTML("beforeend", `
 <div id="video_speed_controller">
-    Speed: <input type="number" id="video_speed_controller_value" value=${document.querySelector("video.video-stream.html5-main-video").playbackRate} oninput="document.querySelector('video.video-stream.html5-main-video').playbackRate = event.target.value;"><button onclick="document.getElementById('video_speed_controller_value').value = 1; document.querySelector('video.video-stream.html5-main-video').playbackRate = document.getElementById('video_speed_controller_value').value;">reset</button>
+    Speed: <input type="number" id="video_speed_controller_value" value=${default_speed} oninput="document.querySelector('video.video-stream.html5-main-video').playbackRate = event.target.value;"><button onclick="document.getElementById('video_speed_controller_value').value = 1; document.querySelector('video.video-stream.html5-main-video').playbackRate = document.getElementById('video_speed_controller_value').value;">reset</button>
 </div>
     `);
 };
