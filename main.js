ac623c69db14100ca06f521a6906d0dd94e405b2
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2022-11-23 12:18:03 +0900

    最初に動画ページ以外を読み込むと動作しないことがあるバグを修正

--- main.js
+++ main.js
@@ -1,19 +1,61 @@
-const video_element = document.querySelector("video.video-stream.html5-main-video");
-let default_speed = video_element ? parseFloat(video_element.playbackRate) : 1;
+const video_selector = "video.video-stream.html5-main-video";
 
-document.body.insertAdjacentHTML("beforeend", `
-<div id="speed_controller">
-    Speed: <input type="number" id="speed_controller_input" value=${default_speed}><button id="speed_reset_button">reset</button>
-</div>
-`);
+/**
+ * Get visible video element which is played now.
+ * @returns {HTMLVideoElement | null} video element or null if no playing video found.
+ */
+const get_video_element = () => {
+    const video_elements_list = [...document.querySelectorAll(video_selector)];
 
-const speed_input = document.getElementById("speed_controller_input");
+    // Get visible video elements.
+    const video_element = video_elements_list.filter((element) => element.offsetParent)[0];
+    return video_element || null;
+};
 
+/**
+ * Set video playback rate.
+ * @param {number} speed
+ */
+const set_video_speed = (speed) => {
+    const video = get_video_element();
+    if (!video) return;
+    video.playbackRate = speed;
+};
+
+/**
+ * Get video playback rate. If visible video isn't found, return 1.
+ * @returns {number} playback rate
+ */
+const get_video_speed = () => {
+    const video = get_video_element();
+    if (!video) return 1;
+    return video.playbackRate;
+};
+
+const default_speed = get_video_speed();
+
+const controller_outer = document.createElement("div");
+controller_outer.id = "speed_controller";
+controller_outer.textContent = "Speed: ";
+
+const speed_input = document.createElement("input");
+speed_input.type = "number";
+speed_input.min = 0;
+speed_input.id = "speed_controller_input";
+speed_input.value = default_speed;
 speed_input.addEventListener("input", () => {
-    video_element.playbackRate = speed_input.value;
+    set_video_speed(speed_input.value);
 });
 
-document.getElementById("speed_reset_button").addEventListener("click", () => {
+const reset_button = document.createElement("button");
+reset_button.id = "speed_reset_button";
+reset_button.textContent = "reset";
+reset_button.addEventListener("click", () => {
     speed_input.value = 1;
-    video_element.playbackRate = speed_input.value;
+    set_video_speed(speed_input.value);
 });
+
+controller_outer.appendChild(speed_input);
+controller_outer.appendChild(reset_button);
+
+document.body.appendChild(controller_outer);
